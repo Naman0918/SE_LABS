c5418c96d4c4509670741912dcbbed656c808bc3
Naman <102941774+Naman0918@users.noreply.github.com>
2023-06-05 17:44:49 +0530

    Add files via upload

---
+++ CG_LAB-main/08-Animation/3D Cube Animate/3D_Cube.c
@@ -0,0 +1,81 @@
+#include <iostream>
+#include <GL/glu.h>
+#include <GL/glut.h>
+using namespace std;
+GLfloat T = 0;
+void MyInit()
+{
+  glClearColor(0, 0, 0, 1);
+  glColor3f(1, 0, 0);
+  glEnable(GL_DEPTH_TEST);
+}
+
+void Spin()
+{
+  T = T + 1;
+  if (T > 360)
+    T = 0;
+  glutPostRedisplay();
+}
+
+void Face(GLfloat A[], GLfloat B[], GLfloat C[], GLfloat D[])
+{
+  glBegin(GL_POLYGON);
+  glVertex3fv(A);
+  glVertex3fv(B);
+  glVertex3fv(C);
+  glVertex3fv(D);
+  glEnd();
+}
+
+void Cube(GLfloat V0[], GLfloat V1[], GLfloat V2[], GLfloat V3[], GLfloat V4[], GLfloat V5[], GLfloat V6[], GLfloat V7[])
+{
+  glColor3f(1, 0, 0);
+  Face(V0, V1, V2, V3);
+  glColor3f(0, 1, 0);
+  Face(V4, V5, V6, V7);
+  glColor3f(0, 0, 1);
+  Face(V0, V3, V7, V4);
+  glColor3f(1, 0, 1);
+  Face(V1, V2, V6, V5);
+  glColor3f(1, 1, 0);
+  Face(V0, V1, V5, V4);
+  glColor3f(0, 1, 1);
+  Face(V3, V2, V6, V7);
+}
+
+void Draw()
+{
+  GLfloat V[8][3] = {
+      {-0.5, 0.5, 0.5},
+      {0.5, 0.5, 0.5},
+      {0.5, -0.5, 0.5},
+      {-0.5, -0.5, 0.5},
+
+      {-0.5, 0.5, -0.5},
+      {0.5, 0.5, -0.5},
+      {0.5, -0.5, -0.5},
+      {-0.5, -0.5, -0.5}
+
+  };
+  glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+  glLoadIdentity();
+  glRotatef(T, 1, 1, 0);
+  Cube(V[0], V[1], V[2], V[3], V[4], V[5], V[6], V[7]);
+
+  glutSwapBuffers();
+}
+
+int main(int C, char *V[])
+{
+  glutInit(&C, V);
+  glutInitWindowPosition(250, 50);
+  glutInitWindowSize(600, 600);
+  glutInitDisplayMode(GLUT_RGB | GLUT_DOUBLE | GLUT_DEPTH);
+  glutCreateWindow("Cube");
+  MyInit();
+  glutDisplayFunc(Draw);
+  glutIdleFunc(Spin);
+  glutMainLoop();
+  return 0;
+}
